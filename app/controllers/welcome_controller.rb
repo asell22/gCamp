ce0395c63d9869429578ff16991785502e755450
asell22 <alexanderjsell@gmail.com>
2014-11-21 14:54:41 -0800

    quotes on page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
 class WelcomeController < ApplicationController
 
   def home
+    @quotes = [
+      ["Arnold Schwarzenegger", "\"Failure is not an option. Everyone has to succeed.\"",],
+      ["Steve Jobs", "\"Your time is limited, so don't waste it living someone else's life\"",],
+      ["Papa John", "\"Better Ingredients, Better Pizza\"",],
+      ]
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
    get "terms" => "welcome#terms"
 
+   get "home" => "welcome#home"
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
