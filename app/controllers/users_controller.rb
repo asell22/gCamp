1379c540dc3057272782f2820e2a32744cb48f30
asell22 <alexanderjsell@gmail.com>
2014-12-11 08:59:01 -0800

    users can be updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,16 @@ class UsersController < ApplicationController
     redirect_to users_path, notice: "User was successfully created!"
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to users_path, notice: "User was successfully updated!"
+  end
+
   private
 
   def user_params
