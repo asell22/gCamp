e2ccd7e373893bf2fee7b86532ba87a23c9d0f4a
asell22 <alexanderjsell@gmail.com>
2014-12-11 12:31:51 -0800

    Can delete users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,12 @@ class UsersController < ApplicationController
     redirect_to users_path, notice: "User was successfully updated!"
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, notice: "User was successfully deleted!"
+  end
+
   private
 
   def user_params
